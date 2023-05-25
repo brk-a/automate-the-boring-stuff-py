b7357558058e9278e2c9e4e6959c07f563a90d17
brk-a <fnjakai@outlook.com>
2023-05-25 09:34:41 +0000

    reset

--- 0x01-learn_py/0-replace_words.py
+++
@@ -1,6 +0,0 @@
-sentence = input(f'Enter your sentence...\n')
-target = input(f'Enter the word you want to remove...\n')
-replacement = input(f'Enter the word you want to replace it with...\n')
-
-print(f'Your sentence: {sentence}\nThe target: {target}\nReplacement: {replacement}\n')
-print(f'{sentence.replace(target, replacement)}')
