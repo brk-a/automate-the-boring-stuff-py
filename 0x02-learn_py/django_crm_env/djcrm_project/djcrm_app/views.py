9cd27f41bf803a0b3bb7a9874ccc03c30d317d0a
brk-a <fnjakai@outlook.com>
2023-06-05 04:06:50 +0000

    navbar and sign in: done

--- 0x02-learn_py/django_crm_env/djcrm_project/djcrm_app/views.py
+++ 0x02-learn_py/django_crm_env/djcrm_project/djcrm_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(req):
+    if req.method == 'POST':
+        username = req.POST['username']
+        password = req.POST['password']
+
+        user = authenticate(req, username=username, password=password)
+        if user is not None:
+            login(req, user=user)
+            messages.success(req, f'Karibu, {username.lower()}!')
+            return redirect('home')
+        else:
+            messages.success(req, f'Login haikufaulu. Tafadhali jaribu tena.')
+            return redirect('home')
+
+    return render(request=req, template_name='main/home.html', context={})
+
+# def login_user(req):
+#     pass
+
+def logout_user(req):
+    logout(req)
+    messages.success(req, f'Logout imefaulu. Kwaheri.')
+    return redirect('home')
+
+def register_user(req):
+    return render(request=req, template_name='main/register.html', context={})
