1f5aab4281a9ea128882fd674676e376b5fe15a1
brk-a <fnjakai@outlook.com>
2023-06-09 03:37:38 +0000

    user register page

--- 0x02-learn_py/django_crm_env/djcrm_project/djcrm_app/views.py
+++ 0x02-learn_py/django_crm_env/djcrm_project/djcrm_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(req):
@@ -28,4 +29,18 @@ def logout_user(req):
     return redirect('home')
 
 def register_user(req):
-    return render(request=req, template_name='main/register.html', context={})
+    if req.method == 'POST':
+        form  = SignUpForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(req, user=user)
+            messages.success(req, f'Karibu, {username.lower()}!')
+            return redirect('home')
+    else:
+        form = SignUpForm() #not passing `req` because user has not filled form, yet
+        return render(request=req, template_name='main/register.html', context={'form':form})
+    
+    return render(request=req, template_name='main/register.html', context={'form': form})
