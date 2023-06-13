c14235f525da9269d834c064bb27aac17c210871
brk-a <fnjakai@outlook.com>
2023-06-13 04:02:03 +0000

    login & signup: inactive page

--- 0x03-learn_py/djom_env/djom_project/djom_app/views.py
+++ 0x03-learn_py/djom_env/djom_project/djom_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(req):
     return render(request=req, template_name='main/index.html', context={})
+
+def contact(req):
+    return render(request=req, template_name='main/contact.html', context={})
