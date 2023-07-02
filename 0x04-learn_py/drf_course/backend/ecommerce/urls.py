5bbc9036e65477c4d7847039ddd858a09948f282
brk-a <fnjakai@outlook.com>
2023-07-02 08:41:50 +0000

    create urls

--- 0x04-learn_py/drf_course/backend/ecommerce/urls.py
+++ 0x04-learn_py/drf_course/backend/ecommerce/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import routers
+from  .views import ItemViewSet, OrderViewSet
 
-urlpatterns = [
+router = routers.DefaultRouter()
+router.register(r'item', viewset=ItemViewSet, basename='item')
+router.register(r'order', viewset=OrderViewSet, basename='order')
+
+urlpatterns = router.urls
+
+urlpatterns += [
     path('api-token-auth/', obtain_auth_token),
 ]
